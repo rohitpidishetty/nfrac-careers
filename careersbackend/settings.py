d7863879be8925aaea3a3d1383855018d542f2e4
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-04-30 23:12:49 -0500

    Update settings.py

--- careersbackend/settings.py
+++ careersbackend/settings.py
@@ -129,7 +129,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://careers-nfrac.web.app"
 ]
 
 
